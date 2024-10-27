9e230ca4cb5aed4ac0729bf2d17743bb6c874614
shashanknv <shashanknv333@gmail.com>
2024-10-27 19:09:37 +0530

    Understand the react flow and structure

--- 01basic_react/src/App.js
+++ 01basic_react/src/App.js
@@ -1,9 +1,12 @@
-
+import Chai from './Chai'
 
 
 function App() {
   return (
-    <h1>Chai aur React</h1>
+    <>
+      <Chai/>
+      <h1>Chai aur React</h1>
+    </>
   );
 }
 
--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,10 +1,16 @@
-
+import Chai from "./chai"
 
 function App() {
   
 
   return (
-    <h1>Chai aur React with Vite | NV</h1>
+    // <h1>Chai aur React with Vite | NV</h1>
+    <>
+      <Chai/>
+      <h1>chai aur react</h1>
+      <p>This is a para</p>
+    </>
+    
   )
 }
 
