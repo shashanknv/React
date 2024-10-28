6bf2a1efa9f7f3a702935aade30e8b1cc33e6285
shashanknv <shashanknv333@gmail.com>
2024-10-28 23:57:07 +0530

    custom react library and jsx

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,13 +1,13 @@
 import Chai from "./chai"
 
 function App() {
-  
+  const username = "chai aur code"
 
   return (
     // <h1>Chai aur React with Vite | NV</h1>
     <>
       <Chai/>
-      <h1>chai aur react</h1>
+      <h1>chai aur react {username}</h1>
       <p>This is a para</p>
     </>
     
--- 01vite_react/src/main.jsx
+++ 01vite_react/src/main.jsx
@@ -1,9 +1,43 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App | Envy</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    // <App />
+    reactElement
+    
+  
 )
